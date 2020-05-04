254f4776995268c97947a975ccf79681721508b0
Jesus Camacaro <jesus.camacaro@u-payments.com>
2020-05-04 16:35:52 -0400

    comprobacion de token

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const secure = require('./secure')
 const  response = require('../../../network/response');
 const controller = require('./index');
 
@@ -8,7 +9,7 @@ const router = express.Router();
 router.get('/', list);  
 router.get('/:id', get);  
 router.post('/', upsert);  
-router.put('/', upsert);  
+router.put('/', secure('update'), upsert);  
 
 // Internal function
 async function list (req, res) {
--- auth/index.js
+++ auth/index.js
@@ -1,10 +1,55 @@
 
 const jwt = require('jsonwebtoken')
+const config = require('../config')
+const secret = config.jwt.secret
 
 const sign = (data) => {
-    return jwt.sign(data, 'secreto')
+    return jwt.sign(data, secret)
+}
+
+const verify = (token) => {
+    return jwt.verify(token, secret)
+}
+
+const check = {
+    own: function (req, owner) {
+        const decoded = decodeHeader(req)
+        console.log(decoded);
+
+        // COMPROBAR SI ES O NO PROPIO
+        if( decoded.id !== owner){
+            throw new Error('No puedes hacer esto')
+        }
+    },
+}
+
+const getToken = (authorization) => {
+    
+    if(!authorization) {
+        throw new Error('No viene token')
+    }
+
+    if( authorization.indexOf('Bearer ') === -1 ) {
+        throw new Error('Formato invalido')
+    }
+
+    let token = authorization.replace('Bearer ', '')
+    return token
+}
+
+
+const decodeHeader = (req) => {
+    
+    const authorization = req.headers.authorization || '';
+    const token = getToken(authorization);
+    const decoded = verify(token)
+
+    req.user = decoded;
+
+    return decoded;
 }
 
 module.exports = {
     sign,
+    check,
 }
