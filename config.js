7d4a1116fbdbdc47e2ac08b115314d8b976a4433
Jesus Camacaro <jesus.camacaro@u-payments.com>
2020-05-04 18:35:16 -0400

    mysql

--- config.js
+++ config.js
@@ -5,5 +5,21 @@ module.exports = {
     },
     jwt: {
         secret: process.env.JWT_SECRET || 'notasecret!',
-    }
+    },
+    mysql: {
+        host: process.env.MYSQL_HOST || 'remotemysql.com',
+        user: process.env.MYSQL_USER || 'C07BE3P7yb',
+        password: process.env.MYSQL_PASSWORD || 'v9oMIoxyFX',
+        database: process.env.MYSQL_DB || 'C07BE3P7yb',
+    },
 }
+
+// Username: 
+
+// Database name: 
+
+// Password: 
+
+// Server: 
+
+// Port: 3306
---
+++ store/mysql.js
@@ -0,0 +1,113 @@
+
+const mysql = require('mysql')
+
+const config = require('../config')
+
+const dbconf = {
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database,
+}
+
+// Conexion
+let connection;
+
+function handleCon() {
+    connection = mysql.createConnection(dbconf)
+
+    connection.connect( (err) => {
+        if( err ){
+            console.error('[db err]', err);
+            setTimeout(handleCon, 2000)
+        } else {
+            console.log('DB connected!');
+        }
+    } )
+
+    connection.on('error', err => {
+        console.error('[db err]', err);
+        
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+            setTimeout(handleCon, 2000)
+        } else {
+            throw err
+        }
+    })
+}
+
+handleCon();
+
+function list (table) {
+    return new Promise ( (resolve, reject) => {
+        connection.query(`SELECT * FROM ${table}`, (error, data) => {
+            
+            if(error) return reject(error)
+
+            resolve(data)
+        })
+    })
+}
+
+function get (table, id) {
+    return new Promise ( (resolve, reject) => {
+        connection.query(`SELECT * FROM ${table} WHERE id = ${id}`, (error, data) => {
+            
+            if(error) return reject(error)
+
+            resolve(data)
+        })
+    })
+}
+
+function insert (table, data) {
+    return new Promise ( (resolve, reject) => {
+        connection.query(`INSERT INTO ${table} SET ?`, data, (error, result) => {
+            
+            if(error) return reject(error)
+
+            resolve(result)
+        })
+    })
+}
+
+function update (table, data) {
+    return new Promise ( (resolve, reject) => {
+        connection.query(`UPDATE ${table} SET ? WHERE id = ?`, [data, data.id], (error, result) => {
+            
+            if(error) return reject(error)
+
+            resolve(result)
+        })
+    })
+}
+
+function query (tabla, query) {  
+    return new Promise( (resolve, reject) => {
+        connection.query(`SELECT * FROM ${tabla} WHERE ?`, query, (error, res) => {       
+            if(error) return reject(error)
+            resolve(res[0] ? {...res[0]} : null)
+        })
+    })
+}
+
+async function upsert (table, data) {
+
+    // revisar esto, lo tome de un comentario porqeu no seria esto
+    // segun los mismo comentarios 
+    const resultado = await get(table, data.id)
+    
+    //  data && data.id
+    if( resultado.length > 0) {
+        return update(table, data)
+    } else {
+        return insert(table, data)
+    }
+}
+
+module.exports = {
+    list,
+    get,
+    upsert,
+    query,
+}
